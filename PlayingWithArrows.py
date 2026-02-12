f7f751b75421fa5688c6f2a027d6e60f525c635d
Gabriel Karloh <97714237+Karloh02@users.noreply.github.com>
2026-02-12 08:58:08 -0300

    Update PlayingWithArrows.py

--- PlayingWithArrows.py
+++ PlayingWithArrows.py
@@ -1,1 +1,12 @@
+#The main idea of this code is to do a game of snail using only python.
+#This has beem made by billion people, however I think this is a great opportunity to improve my skills 
+#The code must have: 
+  #- Record the last position of the snail
+  #- Record the last movement made
+  #- Have a scoring system
+  #- Detect colisions with boundarys and the snail itself
+  #- Have the fruits that extend the snail lenght
+  #- The movement made by the arrow keys on the computer
+  #- Every movement will not be live -> It willl have an iterval of arround 1 second between each movement then it will continue or register the new movement. 
+#With this work made, I will try to implement tis game to a discord bot, where the game will be played on discord chat.
 
